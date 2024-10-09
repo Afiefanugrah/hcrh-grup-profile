bee936684073de79b39917118530fece61240af6
M. Afief Anugrah <mafiefanugrah01@gmail.com>
2024-10-10 00:43:49 +0700

    update baru

---
+++ script.js
@@ -0,0 +1,22 @@
+// const slider = document.querySelector('.slider');
+// const prevButton = document.querySelector('.prev');
+// const nextButton = document.querySelector('.next');
+
+// // Variabel untuk jarak perpindahan slider
+// const profileWidth = 320; // Lebar profil (300px) + margin (20px)
+
+// // Fungsi untuk geser ke kanan
+// nextButton.addEventListener('click', () => {
+//   slider.scrollBy({ 
+//     left: profileWidth, 
+//     behavior: 'smooth' 
+//   });
+// });
+
+// // Fungsi untuk geser ke kiri
+// prevButton.addEventListener('click', () => {
+//   slider.scrollBy({ 
+//     left: -profileWidth, 
+//     behavior: 'smooth' 
+//   });
+// });
